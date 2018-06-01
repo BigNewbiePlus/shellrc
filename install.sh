869711587e4956d392fed6d6cd2048b0292cf839
fangdenghui <fangdenghui@bytedance.com>
2018-06-01 06:21:21 +0000

    添加install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e
 
-SHELLEXE_PATH=$(cd `dirname $0`; pwd)
-
-cat $SHELLEXE_PATH/dotfiles/zshrc > ~/.zshrc
-cat $SHELLEXE_PATH/dotfiles/tmux.conf > ~/.tmux.conf
-cat $SHELLEXE_PATH/dotfiles/basic.vim > ~/.vimrc
-
-source ~/.zshrc
-source ~/.vimrc
-tmux source ~/.tmux.conf
+# install oh my zsh
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Installed the Basic Vim cinfiguration successfully! Enjoy :-)"
